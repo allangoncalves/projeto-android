3097423217426c4dc45e23c1c4ea5b0d8a7a7464
Allan Gonçalves <allangoncalves@outlook.com>
2018-12-06 22:27:37 +0000

    ajuste no layout das chamadas

--- app/src/main/java/com/example/allan/citizenhero/Adapters/CallAdapter.java
+++ app/src/main/java/com/example/allan/citizenhero/Adapters/CallAdapter.java
@@ -1,11 +1,17 @@
 package com.example.allan.citizenhero.Adapters;
 
+
+import com.example.allan.citizenhero.R;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.allan.citizenhero.DTOs.CallDTO;
+
+import org.w3c.dom.Text;
 
 import java.util.Collection;
 import java.util.List;
@@ -13,15 +19,34 @@ import java.util.List;
 public class CallAdapter extends ArrayAdapter<CallDTO> {
 
     private List<CallDTO> calls;
+    private Context context;
 
     public CallAdapter(Context context, int resource, List<CallDTO> calls) {
         super(context, resource, calls);
+        this.context = context;
         this.calls = calls;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        LayoutInflater inflater = LayoutInflater.from(this.context);
+        ItemChamado itemHolder;
+
+        if(convertView == null){
+            convertView = inflater.inflate(R.layout.chamado_item, null);
+            itemHolder = new ItemChamado();
+            itemHolder.textView = (TextView) convertView.findViewById(R.id.endereco_completo_lista);
+            convertView.setTag(itemHolder);
+        }
+        else{
+            itemHolder = (ItemChamado) convertView.getTag();
+        }
+
+        CallDTO call = calls.get(position);
+        itemHolder.textView.setText(call.getFullAddress());
+
+
+        return convertView;
     }
 
     public void updateAll(Collection<? extends  CallDTO> callsFromApi){
@@ -38,5 +63,9 @@ public class CallAdapter extends ArrayAdapter<CallDTO> {
     @Override
     public long getItemId(int position) {
         return calls.indexOf(calls.get(position));
+    }
+
+    private class ItemChamado{
+        TextView textView;
     }
 }
